ca7c4cff18975081c0e37d35f46736096822020d
MaQingyang <qingyangma.cronu@gmail.com>
2026-08-11 20:02:04 +0800

    enhancement to avoid O(n) time complicated in hashing the bytes object

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -4,24 +4,24 @@ from concurrent.futures import ProcessPoolExecutor,as_completed
 import heapq
 import os
 from collections import Counter
-import time
 
 class ReverseBytes:
-    __slots__ = ("data",)
-    def __init__(self, data):
-        self.data = data
+    __slots__ = ("id", "vocab")
+    def __init__(self, id_, vocab):
+        self.id = id_
+        self.vocab = vocab
     def __lt__(self, other):
-        return self.data > other.data  # 反转比较方向
+        return self.vocab[self.id] > self.vocab[other.id]
     def __eq__(self, other):
-        return self.data == other.data
+        return self.id == other.id
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
 ## 重建新堆
-def rebuild_heap(global_dict_pair):
+def rebuild_heap(global_dict_pair,vocab):
     heap = []
     for pair, count in global_dict_pair.items():
-        heapq.heappush(heap, (-count, ReverseBytes(pair[0]), ReverseBytes(pair[1]), pair))
+        heapq.heappush(heap, (-count, ReverseBytes(pair[0],vocab), ReverseBytes(pair[1],vocab), pair))
     return heap
 ## 统计chunk中的字符个数
 def get_statistic(
@@ -29,23 +29,19 @@ def get_statistic(
         end:int,
         input_path:str,
         special_tokens:list[str]
-)->dict[bytes,int]:
+)->dict[tuple[int,...],int]:
     word_dict = {}
-    delimiter="|".join(
-        re.escape(token)
-        for token in special_tokens
-    )
+    delimiter="|".join(re.escape(token) for token in special_tokens)
     with open(input_path,"rb") as f:
         f.seek(start)
-        chunk=f.read(end-start)
-        chunk=chunk.decode("utf-8")
+        chunk=f.read(end-start).decode("utf-8")
 
         segments=re.split(delimiter,chunk)
-
         for segment in segments:
             for match in re.finditer(PAT,segment):
-                word=match.group().encode("utf-8")
-                word_dict[word]=word_dict.get(word,0)+1
+                word_bytes=match.group().encode("utf-8")
+                word_tuple=tuple(word_bytes)
+                word_dict[word_tuple]=word_dict.get(word_tuple,0)+1
     return word_dict
 
 def train_bpe(
@@ -77,22 +73,17 @@ def train_bpe(
         for future in as_completed(futures):
             global_dict.update(future.result())
     print("Finish processing chunks in multithreads")
-    global_dict_tuple={tuple(bytes([c]) for c in k):v for k,v in global_dict.items()}
 ## 单词中的字母两两组合
 ## 以及这个两两组合出现在哪些单词元组中
     global_dict_pair={}
     global_pair_to_word={}
 
-    for word,freq in global_dict_tuple.items():
+    for word,freq in global_dict.items():
         for i in range(len(word)-1):
             pair=(word[i],word[i+1])
             global_dict_pair[pair]=global_dict_pair.get(pair,0)+freq
 
             global_pair_to_word.setdefault(pair,set()).add(word)
-    
-    heap = []
-    for pair, count in global_dict_pair.items():
-        heapq.heappush(heap, (-count, ReverseBytes(pair[0]), ReverseBytes(pair[1]), pair))
 
 ## 初始化vocab
     vocab={}
@@ -100,6 +91,11 @@ def train_bpe(
         vocab[i]=bytes([i])
     for i in range(len(special_tokens)):
         vocab[256+i]=special_tokens[i].encode("utf-8")
+    
+    
+    heap = []
+    for pair, count in global_dict_pair.items():
+        heapq.heappush(heap, (-count, ReverseBytes(pair[0],vocab), ReverseBytes(pair[1],vocab), pair))
 
 ## merge对应的pair
     num_merge=vocab_size-256-len(special_tokens)
@@ -108,7 +104,7 @@ def train_bpe(
     print("Start to merge")
     for i in range(num_merge):
         if len(heap)>2*len(global_dict_pair):
-            heap=rebuild_heap(global_dict_pair)
+            heap=rebuild_heap(global_dict_pair,vocab)
 
         while True:
             neg_count,_,_,pair=heapq.heappop(heap)
@@ -116,20 +112,20 @@ def train_bpe(
             if global_dict_pair.get(pair,0)==count:
                 max_pair=pair
                 break
-        
-        merges.append(max_pair)
-        new_token=max_pair[0]+max_pair[1]
-        vocab[len(vocab)]=new_token
+
+        new_id=len(vocab)
+        merges.append((vocab[max_pair[0]],vocab[max_pair[1]]))
+        vocab[new_id]=vocab[max_pair[0]]+vocab[max_pair[1]]
 
 ## 合并max_pair
         for word in global_pair_to_word[max_pair].copy():
-            freq=global_dict_tuple[word]
+            freq=global_dict[word]
             new_word=[]
             i=0
 ## 提取新word
             while i<len(word):
                 if i+1<len(word) and (word[i],word[i+1])==max_pair:
-                    new_word.append(max_pair[0]+max_pair[1])
+                    new_word.append(new_id)
                     i+=2
                 else:
                     new_word.append(word[i])
@@ -152,16 +148,16 @@ def train_bpe(
                     del global_dict_pair[pair]
                 else:
                     global_dict_pair[pair]=new_count
-                    heapq.heappush(heap,(-new_count,ReverseBytes(pair[0]),ReverseBytes(pair[1]),pair))
+                    heapq.heappush(heap,(-new_count,ReverseBytes(pair[0],vocab),ReverseBytes(pair[1],vocab),pair))
 ## 加上新pair的频率
             for pair,count in new_pairs.items():
                 new_count=global_dict_pair.get(pair,0)+count*freq
                 global_dict_pair[pair]=new_count
-                heapq.heappush(heap,(-new_count,ReverseBytes(pair[0]),ReverseBytes(pair[1]),pair))
+                heapq.heappush(heap,(-new_count,ReverseBytes(pair[0],vocab),ReverseBytes(pair[1],vocab),pair))
 ## 删掉旧word
-            del global_dict_tuple[word]
+            del global_dict[word]
 ## 增加新word
-            global_dict_tuple[new_word]=global_dict_tuple.get(new_word,0)+freq
+            global_dict[new_word]=global_dict.get(new_word,0)+freq
 ## 删掉旧pair对旧word的所有索引
             for pair in old_pairs:
                 if pair in global_pair_to_word:
